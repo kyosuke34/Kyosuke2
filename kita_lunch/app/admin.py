3ca7d1fd7bf2722c3f5f1b44ef239bf3f77e5e3e
kyosuke34 <9mm.vegas@gmail.com>
2024-11-16 08:59:27 +0900

    11/16

--- kita_lunch/app/admin.py
+++ kita_lunch/app/admin.py
@@ -1,30 +1,26 @@
 from django.contrib import admin
 from.models import *
-from .models import Area
+
 class AreaAdmin(admin.ModelAdmin):
     list_display = ['area_id','area_name']
     list_filter = ['area_name']
 admin.site.register(Area,AreaAdmin)
 
 
-
-from .models import Store
-from django.utils.safestring import mark_safe
-class StoreAdmin(admin.ModelAdmin):
-    list_display = [
-        'store_id',
-        'store_name',
-        'address',
-        'price',
-        'image',
-   ]
-    def image(self, obj):
-     return mark_safe('<img src="{}" style="width:100px height:auto;">'.format(obj.photo.url))
-admin.site.register(Store,StoreAdmin)
+# from django.utils.safestring import mark_safe
+# class StoreAdmin(admin.ModelAdmin):
+#     list_display = [
+#         'store_id',
+#         'store_name',
+#         'address',
+#         'price',
+#         'image',
+#    ]
+#     def image(self, obj):
+#      return mark_safe('<img src="{}" style="width:100px height:auto;">'.format(obj.photo.url))
+# admin.site.register(Store,StoreAdmin)
 
 
-
-from .models import AreaStore
 class AreaStoreAdmin(admin.ModelAdmin):
     list_display = [
         'area',
@@ -32,7 +28,7 @@ class AreaStoreAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(AreaStore,AreaStoreAdmin)
-
+admin.site.register(Store)
 admin.site.register(Tag)
 admin.site.register(StoreTag)
 
